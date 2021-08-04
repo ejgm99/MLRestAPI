3c26c61f533aa89bf97b58213d81e84edba82a25
ejgm99 <erickgomez@mymail.mines.edu>
2021-08-04 14:16:54 -0600

    done cleaning up SentimentTracker

--- nlp_api/apps/deepmoji/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class DeepmojiConfig(AppConfig):
-    name = 'deepmoji'
--- nlp_api/apps/deepmoji/urls.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from django.urls import include,path
-
-from . import views
-
-urlpatterns = [
-    path('',views.evaluateTopics),
-    path('class/',views.classTests)
-]
